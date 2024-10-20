c77b5a0b010b3422ddcd2194c1c7a8d057701384
patmorgan1302 <patrickmorgan1302@gmail.com>
2024-10-20 15:27:50 -0600

    fixed multer bug

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
-import Message from '../../Components/Message';
-import Loader from '../../Components/Loader';
+import Message from '../../components/Message';
+import Loader from '../../components/Loader';
 import FormContainer from '../../Components/FormContainer';
 import { toast } from 'react-toastify';
 import {
